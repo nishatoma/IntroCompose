902a66b501639497e5947c7e5eff2532b5af3d6d
Nisha Shlaymoon <nishatoma@gmail.com>
2022-04-06 19:04:53 -0400

    Create Tap Button

--- app/src/main/java/com/example/introcompose/MainActivity.kt
+++ app/src/main/java/com/example/introcompose/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.introcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.example.introcompose.ui.theme.IntroComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,38 +26,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.primary
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 
-@Preview
 @Composable
-fun ShowAge(age: Int = 12) {
-    Text(text = age.toString())
+fun MyApp() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+    color = Color(0xFF546E7A)
+    ) {
+    }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateCircle() {
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(100.dp),
+    shape = CircleShape) {
+        
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap!")
+        }
+
+        
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntroComposeTheme {
-        Column() {
-            Greeting("Android")
-            ShowAge(age = 34)
-        }
-
-
+        MyApp()
     }
 }
